ad16e8856e9614b69f387e021b774f5e90791fc9
clinthof <73394309+clinthof@users.noreply.github.com>
2022-02-23 00:05:18 -0500

    Initialize navLinks in data file

---
+++ src/data.ts
@@ -0,0 +1,25 @@
+interface Links {
+    name: string,
+    path: string
+}
+
+const navLinks: Array<Links> = [
+    {
+        name: '',
+        path: '',
+    },
+    {
+        name: '',
+        path: '',
+    },
+    {
+        name: '',
+        path: '',
+    },
+    {
+        name: '',
+        path: '',
+    },
+];
+
+export { navLinks };
