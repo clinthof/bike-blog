936adf36bef87f14b09e8c988b9153a78b39d119
clinthof <73394309+clinthof@users.noreply.github.com>
2022-02-23 11:20:25 -0500

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Header from '../src/components/Header'
 
 const Home: NextPage = () => {
   return (
     <div>    
       Blog homepage
+    <Header />
     </div>
   )
 }
