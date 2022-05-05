60885278242f2c49382bb2d4e7088bf51aa1dea0
clinthof <73394309+clinthof@users.noreply.github.com>
2022-05-05 17:15:12 -0400

    Add header to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
-import Header from '../src/components/Header'
+import type { NextPage } from 'next';
+import { HeaderContainer } from '../src/containers/header';
 
 const Home: NextPage = () => {
   return (
     <div>    
+      <HeaderContainer />
       Blog homepage
-    <Header />
     </div>
   )
 }
