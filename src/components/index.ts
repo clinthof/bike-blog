0ede86ce1a539c245c567c5d49977e3f8f171703
clinthof <73394309+clinthof@users.noreply.github.com>
2022-05-08 15:41:50 -0400

    Add post and layout to comp lib

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from './button';
 export { default as Header } from './header';
+export { default as Post } from './post';
+export { default as Layout } from './layout';
