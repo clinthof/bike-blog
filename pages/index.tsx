7be71e67300e1c7f37737a72c2888d1dde5aa52f
clinthof <73394309+clinthof@users.noreply.github.com>
2022-05-11 21:49:03 -0400

    Add landing to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
+import { LandingContainer } from '../src/containers/landing';
 
 const Home: NextPage = () => {
   return (
     <>   
-      Blog homepage content
+      <LandingContainer />
     </>
   )
 }
