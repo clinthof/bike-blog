356a658c5b5b4db34fa1c3bcb772ae65138e940d
clinthof <73394309+clinthof@users.noreply.github.com>
2022-05-11 21:51:51 -0400

    Add landing to comp lib

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './button';
 export { default as Header } from './header';
 export { default as Post } from './post';
 export { default as Layout } from './layout';
+export { default as Landing } from './landing';
