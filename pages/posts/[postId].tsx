a13471e1a3bcd8258814c6d9f1de0f39d7fda73c
clinthof <73394309+clinthof@users.noreply.github.com>
2022-05-29 12:46:44 -0400

    Add graphql to query CMS; add basic queries

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,5 +1,11 @@
-import type { NextPage } from 'next'
+import type {
+    NextPage,
+    GetStaticProps,
+    InferGetStaticPropsType,
+} from 'next'
 import { useRouter } from 'next/router'
+import { getPosts, getPostDetails } from '../../services';
+import { PostContainer } from '../../src/containers/post';
 
 const BlogPost: NextPage = () => {
     const { query } = useRouter();
@@ -7,9 +13,27 @@ const BlogPost: NextPage = () => {
 
     return (
         <>
-            Blog post { postId } details
         </>
     )
 }
 
-export default BlogPost
+export default BlogPost;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const data = await getPostDetails(params?.slug as string);
+
+    return {
+        props: { post: data }
+    }
+};
+
+export const getStaticPaths = async () => {
+    const posts = await getPosts();
+
+    return {
+        paths: posts.map(({ node: { postId } }) => ({
+            params: { postId }
+        })),
+        fallback: false,
+    }
+};
