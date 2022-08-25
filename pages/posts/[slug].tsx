8987c3c0dea61f99ee11364a9233821947ba9536
clinthof <73394309+clinthof@users.noreply.github.com>
2022-08-24 23:47:12 -0400

    Dependency updates; formatting

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,40 +1,52 @@
 import type {
-    NextPage,
-    GetStaticProps,
-    GetStaticPaths,
-    InferGetStaticPropsType,
-} from 'next'
-import { useRouter } from 'next/router'
-import { getPosts, getPostDetails } from '../../services';
+  NextPage,
+  GetStaticProps,
+  GetStaticPaths,
+  InferGetStaticPropsType,
+} from 'next';
+import {useRouter} from 'next/router';
+import {getPosts, getPostDetails} from '../../services';
 
-const BlogPost: NextPage = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
-    const { query, asPath } = useRouter();
-    const slug = query.slug;
-    
-    return (
-        <>
-            {post.title}
-        </>
-    )
-}
+const BlogPost: NextPage = ({
+  post,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const {query, asPath} = useRouter();
+  const slug = query.slug;
+
+  if (!post?.title) {
+    return <>Loading...</>;
+  }
+
+  return <h1>{post.title}</h1>;
+};
 
 export default BlogPost;
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const data = await getPostDetails(params?.slug as string);
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  let data = null;
 
-    return {
-        props: { post: data }
+  try {
+    data = await getPostDetails(params?.slug as string);
+  } catch (err) {
+    if (typeof err === 'string') {
+      console.log(`Error:\n${err.toUpperCase()}`);
+    } else if (err instanceof Error) {
+      console.log(`Error:\n${err.message}`);
     }
+  }
+
+  return {
+    props: {post: data},
+  };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const posts = await getPosts();
+  const posts = await getPosts();
 
-    return {
-        paths: posts.map(({ node: { slug } }) => ({
-            params: { slug }
-        })),
-        fallback: false,
-    }
+  return {
+    paths: posts.map(({node: {slug}}) => ({
+      params: {slug},
+    })),
+    fallback: false,
+  };
 };
